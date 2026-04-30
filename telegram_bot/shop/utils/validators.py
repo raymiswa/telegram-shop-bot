71d4c23421e15ba2a40769c3149bd22929e02c36
raymiswa <raymiswa@icloud.com>
2026-04-30 19:38:53 +0300

    Delete telegram_bot directory

--- telegram_bot/shop/utils/validators.py
+++
@@ -1,9 +0,0 @@
-import re
-
-
-def validate_phone(phone: str) -> bool:
-    return bool(re.match(r"^\+7\d{10}$", phone.replace(" ", "").replace("-", "")))
-
-
-def normalize_phone(phone: str) -> str:
-    return re.sub(r"[\s\-\(\)]", "", phone)
